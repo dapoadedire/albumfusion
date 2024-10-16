2acb817fb537a595aae6d8ac235365dfb52d04a1
Dapo Adedire <adedireadedapo19@gmail.com>
2024-10-16 05:59:51 +0100

    use zod and react hook form

--- components/album/AlbumSearch.tsx
+++ components/album/AlbumSearch.tsx
@@ -1,8 +1,15 @@
 import React, { useState } from "react";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { albumSearchSchema } from "@/lib/schema";
+type AlbumSearchFormData = z.infer<typeof albumSearchSchema>;
+
 interface Album {
   id: string;
   name: string;
   artists: { name: string }[];
+  album_type: string;
   images: { height: number; url: string; width: number }[];
 }
 
@@ -11,53 +18,111 @@ interface AlbumSearchProps {
   onAlbumSelect: (album: Album) => void;
 }
 
+interface FilterType {
+  type: "album" | "single" | "all";
+}
+
 const AlbumSearch: React.FC<AlbumSearchProps> = ({
   accessToken,
   onAlbumSelect,
 }) => {
-  const [searchTerm, setSearchTerm] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
   const [searchResults, setSearchResults] = useState<Album[]>([]);
 
-  const searchAlbums = async () => {
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<AlbumSearchFormData>({
+    resolver: zodResolver(albumSearchSchema),
+    defaultValues: {
+      limit: 15,
+      searchTerm: "",
+    },
+  });
+
+  const onSubmit = async (formData: AlbumSearchFormData) => {
+    setLoading(true);
+    setError(null);
+
     try {
       const response = await fetch(
         `https://api.spotify.com/v1/search?q=${encodeURIComponent(
-          searchTerm
-        )}&type=album&limit=15`,
+          formData.searchTerm
+        )}&type=album&limit=${formData.limit}`,
         {
           headers: {
             Authorization: `Bearer ${accessToken}`,
           },
         }
       );
-      const data = await response.json();
-      console.log(data.albums.items);
-      const albumOnly = data.albums.items.filter(
-        (album: any) => album.album_type === "album"
-      );
-      setSearchResults(albumOnly);
-    } catch (error) {
-      console.error("Error searching albums:", error);
+
+      if (!response.ok) {
+        throw new Error("Failed to search albums");
+      }
+
+      const responseData = await response.json();
+
+      const albums = responseData.albums.items as Album[];
+
+      setSearchResults(albums);
+    } catch (err: any) {
+      setError(err.message || "An error occurred while searching.");
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
     <div>
-      <div className="flex items-center">
-        <input
-          type="text"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          placeholder="Search for albums"
-          className="border p-2 border-gray-300 rounded-md mr-2 w-full"
-        />
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+        <div className="flex flex-col space-y-2">
+          <label htmlFor="searchTerm" className="font-medium text-gray-700">
+            Search Term
+          </label>
+          <input
+            type="text"
+            id="searchTerm"
+            {...register("searchTerm")}
+            placeholder="Search for albums"
+            className="border p-2 border-gray-300 rounded-md w-full"
+          />
+          {errors.searchTerm && (
+            <p className="text-red-500 text-sm">{errors.searchTerm.message}</p>
+          )}
+        </div>
+
+        <div className="flex flex-col space-y-2">
+          <label htmlFor="limit" className="font-medium text-gray-700">
+            Limit Results
+          </label>
+          <select
+            id="limit"
+            {...register("limit", { valueAsNumber: true })}
+            className="border p-2 border-gray-300 rounded-md w-full"
+          >
+            <option value={10}>10</option>
+            <option value={15}>15</option>
+            <option value={20}>20</option>
+          </select>
+          {errors.limit && (
+            <p className="text-red-500 text-sm">{errors.limit.message}</p>
+          )}
+        </div>
+
         <button
-          onClick={searchAlbums}
-          className="bg-blue-500 text-white p-2 rounded w-[100px]"
+          type="submit"
+          className="bg-blue-500 text-white p-2 rounded w-min"
         >
-          Search
+          {loading ? "Searching..." : "Search"}
         </button>
-      </div>
+      </form>
+
+      {error && <p className="text-red-500 mt-4">{error}</p>}
+
+      <h2 className="text-lg font-semibold mt-4">Search Results</h2>
       <ul className="mt-4">
         {searchResults.map((album) => (
           <li key={album.id} className="mb-2">
--- components/playlist/PlaylistForm.tsx
+++ components/playlist/PlaylistForm.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
-
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { playlistSchema } from "@/lib/schema";
 interface PlaylistFormProps {
   onCreatePlaylist: (
     name: string,
@@ -8,68 +11,77 @@ interface PlaylistFormProps {
   ) => Promise<void>;
 }
 
-const PlaylistForm: React.FC<PlaylistFormProps> = ({ onCreatePlaylist }) => {
-  const [playlistName, setPlaylistName] = useState("");
-  const [playlistDescription, setPlaylistDescription] = useState("");
-  const [publicPlaylist, setPublicPlaylist] = useState(true);
-  const [isLoading, setIsLoading] = useState(false); // Loading state
+type PlaylistFormData = z.infer<typeof playlistSchema>;
 
-  const handleCreatePlaylist = async () => {
-    setIsLoading(true); // Start loading
+const PlaylistForm: React.FC<PlaylistFormProps> = ({ onCreatePlaylist }) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<PlaylistFormData>({
+    resolver: zodResolver(playlistSchema),
+  });
+
+  const onSubmit = async (data: PlaylistFormData) => {
+    setIsLoading(true);
     try {
-      await onCreatePlaylist(playlistName, playlistDescription, publicPlaylist);
+      await onCreatePlaylist(data.name, data.description, data.isPublic);
     } finally {
-      setIsLoading(false); // Stop loading after the process finishes
+      setIsLoading(false);
     }
   };
 
   return (
-    <div>
-      <label htmlFor="playlist-name" className="block font-bold mt-4">
-        Playlist Name
-      </label>
-      <input
-        id="playlist-name"
-        type="text"
-        value={playlistName}
-        onChange={(e) => setPlaylistName(e.target.value)}
-        placeholder="Enter playlist name"
-        className="border p-2 w-96 border-gray-300"
-      />
-      <label htmlFor="playlist-description" className="block font-bold mt-4">
-        Playlist Description
-      </label>
-      <input
-        id="playlist-description"
-        type="text"
-        value={playlistDescription}
-        onChange={(e) => setPlaylistDescription(e.target.value)}
-        placeholder="Enter playlist description"
-        className="border border-gray-300 p-2 w-96"
-      />
-      <div className="flex items-center my-4 gap-2">
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="mb-4">
+        <label className="block font-bold mb-2">Playlist Name</label>
         <input
-          id="public-playlist"
-          type="checkbox"
-          className="border p-2"
-          checked={publicPlaylist}
-          onChange={(e) => setPublicPlaylist(e.target.checked)}
+          type="text"
+          {...register("name")}
+          placeholder="Enter playlist name"
+          className="border p-2 w-96 border-gray-300"
         />
-        <label htmlFor="public-playlist" className="block font-bold">
-          Public Playlist
+        {errors.name && (
+          <p className="text-red-500 text-sm">{errors.name.message}</p>
+        )}
+      </div>
+
+      <div className="mb-4">
+        <label className="block font-bold mb-2">Playlist Description</label>
+        <input
+          type="text"
+          {...register("description")}
+          placeholder="Enter playlist description"
+          className="border p-2 w-96 border-gray-300"
+        />
+        {errors.description && (
+          <p className="text-red-500 text-sm">{errors.description.message}</p>
+        )}
+      </div>
+
+      <div
+        className="mb-4 
+      "
+      >
+        <label
+          className="font-bold flex gap-2
+      justify-start items-center "
+        >
+          <input type="checkbox" {...register("isPublic")} />
+          <span>Public Playlist</span>
         </label>
       </div>
+
       <button
-        onClick={handleCreatePlaylist}
-        className={` text-white p-2 rounded mt-4 
-            ${isLoading? "bg-green-300": "bg-green-500"}
-            `}
-        disabled={isLoading} // Disable button when loading
+        type="submit"
+        className="bg-blue-500 text-white py-2 px-4 rounded"
+        disabled={isLoading}
       >
-        {isLoading ? "Creating..." : "Create Playlist"}{" "}
-        {/* Show "Creating..." when loading */}
+        {isLoading ? "Creating..." : "Create Playlist"}
       </button>
-    </div>
+    </form>
   );
 };
 
